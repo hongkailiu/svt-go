9c90350c8ac12da8c74d1475d4716ba6f76079c2
Hongkai Liu <hongkailiu@users.noreply.github.com>
2017-07-07 13:18:27 -0400

    Move log code into its own pkg

---
+++ log/log.go
@@ -0,0 +1,45 @@
+package log
+
+import (
+	"github.com/op/go-logging"
+	"os"
+)
+
+var log = logging.MustGetLogger("svt")
+
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+func init() {
+	backend := logging.NewLogBackend(os.Stdout, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, format)
+	//backend1Leveled := logging.AddModuleLevel(backend1)
+	//backend1Leveled.SetLevel(logging.ERROR, "")
+	//logging.SetBackend(backend1Leveled, backend1Formatter)
+	logging.SetBackend(backendFormatter)
+}
+
+func Debug(args ...interface{}) {
+	log.Debug(args)
+}
+
+func Info(args ...interface{}) {
+	log.Info(args)
+}
+
+func Notice(args ...interface{}) {
+	log.Notice(args)
+}
+
+func Warning(args ...interface{}) {
+	log.Warning(args)
+}
+
+func Error(args ...interface{}) {
+	log.Error(args)
+}
+
+func Critical(args ...interface{}) {
+	log.Critical(args)
+}
--- main/svt.go
+++ main/svt.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"github.com/op/go-logging"
-	"os"
-)
-
-var log = logging.MustGetLogger("svt")
-
-var format = logging.MustStringFormatter(
-	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+	"github.com/hongkailiu/svt-go/log"
 )
 
 func main() {
-	backend := logging.NewLogBackend(os.Stderr, "", 0)
-	backendFormatter := logging.NewBackendFormatter(backend, format)
-	//backend1Leveled := logging.AddModuleLevel(backend1)
-	//backend1Leveled.SetLevel(logging.ERROR, "")
-	//logging.SetBackend(backend1Leveled, backend1Formatter)
-	logging.SetBackend(backendFormatter)
 
 	log.Debug("secret")
 	log.Info("info1")
 	log.Notice("notice")
 	log.Warning("warning")
 	log.Error("err")
-	log.Critical("crit")
+	log.Critical("crit000")
 }

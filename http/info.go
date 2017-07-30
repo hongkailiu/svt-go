4cca7a9ff0805768c38a3ada435a409de1b62cb3
Hongkai Liu <hongkailiu@users.noreply.github.com>
2017-07-30 04:37:00 -0400

    Get Ips

--- http/info.go
+++ http/info.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"github.com/hongkailiu/svt-go/log"
 	"io/ioutil"
+	"net"
 )
 
 const (
@@ -22,7 +23,7 @@ func GetInfo() *info {
 
 	i := info{}
 	i.Version = getVersion()
-	i.Ips = []string{"192.168.31.163"}
+	i.Ips = getIps()
 	i.Now = time.Now()
 	return &i
 }
@@ -36,8 +37,35 @@ func getVersion() string {
 
 	text, err := ioutil.ReadFile(dir + string(os.PathSeparator) + VersionFile)
 	if err != nil {
-		log.Critical("aaa" + err.Error())
+		log.Critical(err.Error())
 		return err.Error()
 	}
 	return string(text)
+}
+
+func getIps() []string {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		log.Critical(err.Error())
+		return []string{err.Error()}
+	}
+	// handle err
+	result := []string{}
+	for _, i := range interfaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			result = append(result, err.Error())
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			result = append(result, ip.String())
+		}
+	}
+	return result
 }

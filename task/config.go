bf17c65f1ad35c8049a4a6e98ab0af3169ef789b
Hongkai Liu <hongkailiu@users.noreply.github.com>
2017-07-31 23:49:31 -0400

    Parse yaml config file

---
+++ task/config.go
@@ -0,0 +1,74 @@
+package task
+
+import (
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+)
+
+func LoadFromBytes(source []byte) (*Config, error) {
+	var config Config
+	err := yaml.Unmarshal(source, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+func LoadFromFile(file string) (*Config, error) {
+	source, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	return LoadFromBytes(source)
+}
+
+type Config struct {
+	Projects   []Project `yaml:"projects"`
+	Quotas     []Quota `yaml:"quotas"`
+	TuningSets []TuningSet `yaml:"tuningsets"`
+}
+
+type Project struct {
+	Number   int `yaml:"num"`
+	Basename string `yaml:"basename"`
+	Tuning   string `yaml:"tuning"`
+	Quota    string `yaml:"quota"`
+	Users    []User `yaml:"users"`
+	Templates    []Template `yaml:"templates"`
+}
+
+type User struct {
+	Number       int `yaml:"num"`
+	Role         string `yaml:"role"`
+	Basename     string `yaml:"basename"`
+	Password     string `yaml:"password"`
+	UserPassFile string `yaml:"userpassfile"`
+}
+
+type Template struct {
+	Number   int `yaml:"num"`
+	File     string `yaml:"file"`
+	Parameters    []Parameter `yaml:"parameters"`
+}
+
+type Parameter struct {
+	EnvValue     string `yaml:"ENV_VALUE"`
+}
+
+type Quota struct {
+	Name string `yaml:"name"`
+	File string `yaml:"file"`
+}
+
+type TuningSet struct {
+	Name            string `yaml:"name"`
+	PodsInTuningSet struct {
+				RateLimit struct {
+						  Delay string `yaml:"delay"`
+					  } `yaml:"rate_limit"`
+				Stepping  struct {
+						  StepSize int `yaml:"stepsize"`
+						  Pause    string `yaml:"pause"`
+					  } `yaml:"stepping"`
+			}   `yaml:"pods"`
+}
---
+++ task/config_test.go
@@ -0,0 +1,64 @@
+package task
+
+import (
+	"testing"
+	"github.com/hongkailiu/svt-go/log"
+	"github.com/stretchr/testify/assert"
+	"os"
+)
+
+func TestRootHandler(t *testing.T) {
+
+	assert := assert.New(t)
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	configP, err := LoadFromFile(dir+"/../conf/pyconfigMasterVirtScalePause.yaml")
+	assert.Nil(err)
+	config := *configP
+	assert.NotNil(config)
+
+	assert.Len(config.Projects, 1)
+	p := config.Projects[0]
+	assert.Equal(3, p.Number)
+	assert.Equal("default", p.Tuning)
+	assert.Equal("c", p.Basename)
+
+	assert.Len(p.Users, 1)
+	user := p.Users[0]
+	assert.Equal(1, user.Number)
+	assert.Equal("demo", user.Basename)
+	assert.Equal("admin", user.Role)
+	assert.Equal("demo", user.Password)
+	assert.Equal("/etc/origin/openshift-passwd", user.UserPassFile)
+
+	assert.Len(p.Templates, 7)
+	t0 := Template{Number:3, File:"./content/build-config-template.json", Parameters:nil}
+	t1 := Template{Number:6, File:"./content/build-template.json", Parameters:nil}
+	t2 := Template{Number:1, File:"./content/image-stream-template.json", Parameters:nil}
+	t3 := Template{Number:2, File:"./content/deployment-config-1rep-pause-template.json",
+		Parameters:[]Parameter{{EnvValue:"asodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij12"}}}
+	t4 := Template{Number:1, File:"./content/deployment-config-2rep-pause-template.json",
+		Parameters:[]Parameter{{EnvValue:"asodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij0emc2oed2ed2ed2e2easodfn209e8j0eij12"}}}
+	t5 := Template{Number:20, File:"./content/ssh-secret-template.json"}
+	t6 := Template{Number:3, File:"./content/route-template.json"}
+	assert.Contains(p.Templates, t0)
+	assert.Contains(p.Templates, t1)
+	assert.Contains(p.Templates, t2)
+	assert.Contains(p.Templates, t3)
+	assert.Contains(p.Templates, t4)
+	assert.Contains(p.Templates, t5)
+	assert.Contains(p.Templates, t6)
+
+	assert.Len(config.Quotas, 1)
+	q := Quota{Name:"default"}
+	assert.Contains(config.Quotas, q)
+
+	assert.Len(config.TuningSets, 1)
+	ts := TuningSet{Name:"default"}
+	ts.PodsInTuningSet.Stepping.StepSize = 5
+	ts.PodsInTuningSet.Stepping.Pause = "10 s"
+	ts.PodsInTuningSet.RateLimit.Delay = "250 ms"
+	assert.Contains(config.TuningSets, ts)
+}

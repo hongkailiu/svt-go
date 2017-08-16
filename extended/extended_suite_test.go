78ca3bb27ef96186895b8d8af8ddaa575922a320
Hongkai Liu <hongkailiu@users.noreply.github.com>
2017-08-15 20:48:09 -0400

    Generate junit report

--- extended/extended_suite_test.go
+++ extended/extended_suite_test.go
@@ -5,9 +5,14 @@ import (
 	. "github.com/onsi/gomega"
 
 	"testing"
+	"github.com/onsi/ginkgo/reporters"
+	"fmt"
+	"github.com/onsi/ginkgo/config"
 )
 
 func TestExtended(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Extended Suite")
+	junitReporter := reporters.NewJUnitReporter(fmt.Sprintf("junit_%d.xml", config.GinkgoConfig.ParallelNode))
+	//RunSpecs(t, "Extended Suite")
+	RunSpecsWithDefaultAndCustomReporters(t, "Extended Suite", []Reporter{junitReporter})
 }
